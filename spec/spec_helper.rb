b9c7beeff3e1941500b1af2d57e9aa00fd2d3045
aaronliew <aaron@favoritemedium.com>
2016-04-06 11:43:02 +0800

    Adds show action the users controller

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,24 +21,13 @@ ActiveRecord::Migration.maintain_test_schema!
 require "bundler/setup"
 ::Bundler.require(:default, :test)
 
-require "shoulda/matchers"
-::Shoulda::Matchers.configure do |config|
+Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     # Choose a test framework:
     with.test_framework :rspec
-    #with.test_framework :minitest
-    #with.test_framework :minitest_4
-    #with.test_framework :test_unit
-
-    # Choose one or more libraries:
-    with.library :active_record
-    with.library :active_model
-    #with.library :action_controller
-    # Or, choose the following (which implies all of the above):
-    #with.library :rails
+    with.library :rails
   end
 end
-
 
 RSpec.configure do |config|
   # ## Mock Framework
