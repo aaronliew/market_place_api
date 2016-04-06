197f5c16edb0c31bf932644e068fce061fdd1fa7
aaronliew <aaron@favoritemedium.com>
2016-04-06 10:09:55 +0800

    test api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+require 'api_constraints'
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -57,7 +58,9 @@ Rails.application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/'  do
-    # We are going to list our resources here
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+    end
   end
-  
 end
