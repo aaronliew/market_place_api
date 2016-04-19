d301573314a4ad4ed8255556a57e397b21e0ed24
aaronliew <aaron@favoritemedium.com>
2016-04-19 11:29:17 +0800

    Chapter 3 refactoring test

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ MarketPlaceApi::Application.routes.draw do
   mount SabisuRails::Engine => "/sabisu_rails"
   devise_for :users
   # Api definition
+  #for better understanding of this => http://stackoverflow.com/questions/9627546/api-versioning-for-rails-routes
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/'  do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
 describe Api::V1::UsersController do
-  before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
-
+  
   describe "GET #show" do
     before(:each) do
       @user = FactoryGirl.create :user
@@ -102,4 +101,6 @@ describe Api::V1::UsersController do
 	  it { should respond_with 204 }
 
   end
+
+
 end
